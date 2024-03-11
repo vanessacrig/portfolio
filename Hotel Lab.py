d31852a6603f1c6a8be598ddf77f5bd9fd82f8cb
vanessacrig <163062755+vanessacrig@users.noreply.github.com>
2024-03-11 18:13:40 -0400

    Add files via upload

---
+++ Hotel Lab.py
@@ -0,0 +1,102 @@
+# Hotel Lab.py
+#IT1114/W03
+#Vanessa Crighton
+#Lab 4
+# 09/18/2022
+#This program determines the cost of a hotel stay to include meals and excursions.
+#Resources: Module 5 lecture, Ex10 - Ex13, chegg
+
+
+
+
+#define room costs
+n =0
+rt = 0
+rp = 0
+nop = 0
+
+def rc(n, rt):
+	if rt == 1: #one king bed
+		rc = (n * 350)
+		return rc
+	elif rt == 2:
+		rc = (n * 375)
+		return rc
+	elif rt == 3: 
+		rc = (n * 475)
+		return rc
+	elif rt == 4: 
+		rc = (n * 525)
+		return rc
+
+#define meal cost
+mc = 0
+nb = 0
+tip = 0
+tm = 0
+nd = 0
+
+def mc(nb, nd):
+	mc = ((nb * 25) + (nd * 75))
+	tip = (mc * 0.15)
+	tm = (tip + mc)
+	return tm
+
+
+
+#excursions
+tec = 0
+pc = 0
+sk = 0
+gh = 0
+bd = 0
+
+def tec(pc, sk, gh, bd, nop):
+	tec = 0
+	if pc == "y":
+		tec = (tec + 50)
+	if sk == "y":
+		tec = (tec + (25 * nop))
+	if gh == "y":
+		tec = (tec + (17 * nop))
+	if bd == "y":
+		tec = (tec + 200)
+	return tec
+	
+#inputs
+n = int(input("Number of Nights: "))
+nop = int(input("Number of Guests: "))
+#rooms
+print("Room Types:")
+print("(1) - One King Bed ($350/night)")
+print("(2) - Two Queen Beds ($375/night)")
+print("(3) - King Suite ($475/night)")
+print("(4) - Queen Suite ($525/night)")
+rt = int(input("Choose Room Type: "))
+#meals
+print("Brunch: $25")
+print("Dinner: $75")
+print("* 15% gratuity will be added automatically *")
+nb = int(input("Number of Brunches: "))
+nd = int(input("Number of Dinners: "))
+#Excursions
+print("Excursions: ")
+print("Picnic: $50")
+print("Snorkeling: $25 per person")
+print("Guided Hike: $17 per person")
+print("Boat Dinner (for 2): $200")
+pc = input("Would you like to go on a picnic? [y/n]: ")
+sk = input("Would you like to go snorkeling? [y/n]: ")
+gh = input("Would you like to go on a guided hike? [y/n]: ")
+bd = input("Would you like to have dinner on a boat? [y/n]: ")
+
+#bill
+print("Summary of Charges")
+tr = rc(n, rt)
+print("Room: ", tr)
+fmc = (mc (nb, nd) * nop)
+print("Meals: ", fmc)
+fec = tec(pc, sk, gh, bd, nop)
+print("Excursions: ", fec)
+gt = tr + fmc + fec
+print("Total Amount: ", gt)
---
+++ Inheritance_Lab.py
@@ -0,0 +1,119 @@
+#Inheritance Lab.py
+#IT1114/ W03
+#Vanessa Crighton
+#Lab 7
+#10/16/2022
+#Purpose: to create a parent class that is to be inherited by two child classes.
+
+
+from datetime import datetime
+
+
+#Parent class
+class Employee:
+	def __init__(self, employee_number, office_number, name, birthdate):
+			self.employee_number = employee_number
+			self.office_number = office_number
+			self.name = name
+			self.birthdate = birthdate
+			
+		#Variables
+	employee_number = 0
+	office_number = 0
+	name = "First Last"
+	birthday = date.today()
+		
+		#set employee number
+	def getEmployee_number(self):
+				return self.employee_number
+	def setEmployee_number(self, employee_number):
+				self.employee_number = employee_number
+		#set office number 		
+	def getOffice_number(self):
+				return self.office_number
+	def setOffice_number(self, office_number):
+				if office_number < 100 or office_number > 500:
+					return False
+				else:
+					self.office_number = office_number
+					return True
+		#set name
+	def get_name(self):
+				return self.name
+	def set_name(self, name):
+				self.name = name
+			
+		#birthday
+	def get_birthdate(self):
+			return self.Birthdate
+	
+	def set_birthdate(self, Birthdate):
+			if Birthdate.month < 1 or Birthdate.month > 12 or Birthdate.day < 1 or Birthdate.day > 31:
+				return False
+			else:
+				self.Birthdate = Birthdate
+				return True
+			
+	def is_birthday(self):
+			currentDate = date.today() #Get current date
+			if currentDate.month == self.Birthdate.month and currentDate.day == self.Birthdate.day:
+				return True
+			else:
+				return False
+	
+		
+#1st child class
+		
+		
+class HourlyEmployee(Employee):
+	def __init__(self, employee_number, office_number, name, birthdate, hourly_salary, Overtime_hourly_salary, hours_worked, overtime_hours_worked):
+		Employee(<#employee_number#>, <#office_number#>, <#name#>, <#birthdate#>)
+		self.hourly_salary = hourly_salary
+		self.OverTime_hourly_salary = OverTime_hourly_salary
+		self.hours_worked = hours_worked
+		self.overtime_hours_worked = overtime_hours_worked
+		
+	#Variables
+	hourly_employee_salary = 0
+	OverTime_hourly_salary = 0
+	hours_worked = 0
+	overtime_hours_worked = 0
+	
+	def getHours_worked(self):
+		return self.hours_worked
+	
+	def add_hours(self, hours):
+		if hours <= 9:
+			self.hours_worked += hours
+		else:
+			self.hours_worked +=9
+			self.overTimeHours_worked += hours - 9
+			
+	def getOverTime_hours(self):
+		return self.overtimeHours_worked
+	def get_salary(self):
+		return (self.hours_worked * self.hourly_salary) + (self.overtime_hours_worked * self.OverTime_hourly_salary)
+	
+	
+#2nd child class
+class CommissionEmployee(Employee):
+	def __init__(self, employee_number, office_number, name, birthdate, commission_percentage, total_sales): 
+		Employee(<#employee_number#>, <#office_number#>, <#name#>, <#birthdate#>)
+		self.commission_percentage = commission_percentage
+		self.total_sales = total_sales
+		
+	def get_commission(self):
+		return self.commission_percentage
+	
+	def get_total_sales(self):
+		return self.total_sales
+	
+	def add_sales(self, sales):
+		self.total_sales += sales
+		
+	def get_salary(self):
+		salary = self.commission_percentage * self.total_sales
+		if self.total_sales > 300000:
+			salary = salary + (salary * 0.03)
+		return salary
+	
